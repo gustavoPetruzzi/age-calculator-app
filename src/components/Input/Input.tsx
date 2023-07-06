175dd6cd18945c14cd7e69a8331c005045b1b862
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2023-07-06 14:29:03 -0300

    Add placeholder to input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -8,15 +8,16 @@ interface InputProps {
   errorMessage: string;
   onBlur: (value: string) => void,
   label: string;
+  placeholder: string;
   style?: any;
 }
 
 const Input: Component<InputProps> = (props) => {
-  const [local, others] = splitProps(props, ["label", "onBlur", "hasError", "errorMessage", "min", "max"]);
+  const [local, others] = splitProps(props, ["label", "onBlur", "hasError", "errorMessage", "min", "max", "placeholder"]);
   return (
     <label class={styles['custom-label']} {...others}>
       {local.label}
-      <input onBlur={(e) => local.onBlur(e.target.value)} type="number" min={local.min} max={local.max} />
+      <input onBlur={(e) => local.onBlur(e.target.value)} type="number" min={local.min} max={local.max} placeholder={local.placeholder} />
       {local.hasError && <span>{local.errorMessage}</span> }
     </label>
   );
