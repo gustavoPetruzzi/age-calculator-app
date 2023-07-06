3038dfa36365f35ff17bf3a063bf9d06a31dae14
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2023-07-06 16:28:12 -0300

    Import Component's type instead of importing the whole component

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -1,4 +1,4 @@
-import { Component } from 'solid-js';
+import type { Component } from 'solid-js';
 import styles from './Separator.module.css';
 
 const Separator: Component<{isDisabled: boolean}> = (props) => {
