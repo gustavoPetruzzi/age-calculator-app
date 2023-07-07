d839568b8d35ecda31084e2db58ed2bb5fde8b3e
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2023-07-07 11:46:21 -0300

    Add onClick event in Separator component

--- src/components/Separator/Separator.tsx
+++ src/components/Separator/Separator.tsx
@@ -1,11 +1,16 @@
 import type { Component } from 'solid-js';
 import styles from './Separator.module.css';
 
-const Separator: Component<{isDisabled: boolean}> = (props) => {
+interface SeparatorProps {
+  isDisabled: boolean;
+  onClick: () => void;
+}
+
+const Separator: Component<SeparatorProps> = (props) => {
   return (
     <div class={styles.container}>
       <div class={styles.separator}></div>
-      <div class={styles.button}></div>
+      <div class={styles.button} onClick={props.onClick}></div>
     </div>
   )
 }
