3e8f806ae7ec536b957e5badfd520c95c7fe6731
Gustavo Petruzzi <gustavopetruzzi19@gmail.com>
2023-07-08 11:54:22 -0300

    Implement age display component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { For, type Component } from 'solid-js';
+import { For, type Component, createSignal } from 'solid-js';
 import styles from './App.module.css';
 import Input from './components/Input/Input';
 import Separator from './components/Separator/Separator';
-import { useStore, updateInput } from './store/useStore';
+import { useStore, updateInput, calculate } from './store/useStore';
+import AgeDisplay from './components/AgeDisplay/AgeDisplay';
 const App: Component = () => {
   const inputDates = useStore();
+  const [years, setYears] = createSignal("--");
+  const [months, setMonths] = createSignal("--");
+  const [days, setDays] = createSignal("--");
+  
+  const clickHandler = () => {
+    const {years, months, days } = calculate();
+    setDays(days);
+    setMonths(months);
+    setYears(years);
+  }
+  const isDisabled = !inputDates[0].hasError && !inputDates[1].hasError && !inputDates[2].hasError;
   return (
     <main class={styles.App}>
       <div class={styles.container}>
@@ -23,7 +35,8 @@ const App: Component = () => {
             }
           </For>
         </div>
-        <Separator isDisabled={false} />
+        <Separator isDisabled={isDisabled} onClick={clickHandler} />
+        <AgeDisplay years={years()} months={months()} days={days()} />
       </div>
     </main>
   );
